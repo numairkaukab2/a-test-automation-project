a39e448f54bb0e24ae757d41f2897350bc06c00b
Numair <numairkaukab2@gmail.com>
2025-06-10 13:50:12 +0500

    API Test Add Post

--- test_api/test_api.py
+++ test_api/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-def test_api():
+def test_api_get():
     response = requests.get("https://jsonplaceholder.typicode.com/users/1")
 
     r_body = response.json()
@@ -11,4 +11,12 @@ def test_api():
     assert r_body["name"] == "Leanne Graham"
     assert r_body["company"]["bs"] == "harness real-time e-markets"
     
+def test_api_post():
+    response = requests.post("https://jsonplaceholder.typicode.com/posts", data={
+    "userId": 1,
+    "title": "My blog post title",
+    "body": "This is the text of my latest blog post."
+    })
     
+    assert response.status_code == 201
+    assert type(response.json()["id"]) == int
